854926482a4da301abf375c1089cf7216a9b26de
jatesun <jatesun111@gmail.com>
2024-08-28 11:32:52 +0800

    2024年08月26日 chp6完成

--- chp6.js
+++ chp6.js
@@ -9,3 +9,15 @@ for (const [idx, element] of a.entries()) {
     console.log(idx);
     console.log(element);
 }
+// map
+const m1 = new Map([
+    ["key1", "val1"],
+    ["key2", "val2"],
+    ["key3", "val3"]
+]);
+console.log(m1.size); // 3
+//function
+const m2 = new Map();
+console.log(m2.has("firstName"));
+console.log(m2.get("firstName"));
+m2.set("firstName", "matt").set("lastName", "Frisbie");
